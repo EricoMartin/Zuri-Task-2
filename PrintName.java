fe4d639adc9be98ef835d6549a5add68410c43f0
EricoMartin <martinirex@yahoo.co.uk>
2021-08-17 15:28:52 +0100

    new commit

---
+++ PrintName.java
@@ -0,0 +1,12 @@
+package com.basebox;
+
+/**
+ * {@PrintName} prints a String name.
+ */
+
+public class PrintName {
+    static String name  = "Eric Ibu";
+    public static void main(String[] args){
+        System.out.println(name);
+    }
+}
